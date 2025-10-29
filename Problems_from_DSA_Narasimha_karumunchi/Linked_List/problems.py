e4fe8378acdbbd755ca54399508be0b61b0e41b0
NAGESH V KATTIMANI <nageshvk2003@gmail.com>
2025-10-29 20:25:52 +0530

    Add files via upload

--- Problems_from_DSA_Narasimha_karumunchi/Linked_List/problems.py
+++ Problems_from_DSA_Narasimha_karumunchi/Linked_List/problems.py
@@ -25,6 +25,47 @@ class LL_Problems:
 
         return slow_node.data 
     
+# problem 2 - Display the nodes from end
+    def print_nodes_from_end(self):
+
+        # case 1 : If list is empty
+        if self.L_List.head == None:
+            return -1
+
+        stack = []
+        current = self.L_List.head
+
+        while current != None:
+            stack.append(current.data)
+            current = current.next
+        
+        while stack:
+            print(stack.pop())
+
+# problem 3 - Check whether linked list length us even or odd(P-29)
+    def check_length(self):
+        # find the length of the linked list and find its even or odd
+        
+        # approach:
+        # move a pointer in 2 steps at a time
+        # if its pointing to a node its odd
+        # else if pointing to None its even
+
+        # case 1 : If list is empty
+        if self.L_List.head == None:
+            return -1
+
+
+        current_node = self.L_List.head
+
+        while current_node and current_node.next != None:
+            current_node = current_node.next.next
+
+        if current_node == None:
+            print("The Length of Linked list is even")
+        else:
+            print("The Length of Linked list is odd")
+
 
 if __name__ == "__main__":
 
@@ -39,4 +80,31 @@ if __name__ == "__main__":
         get_middle = LL_Problems(L_List)
         print(get_middle.find_middle())
 
-    find_middle_test()
+    # find_middle_test()
+
+    def print_nodes_from_end_test():
+        L_List = SinglyLinkedList()
+        L_List.push(10)
+        L_List.push(20)
+        L_List.push(30)
+        L_List.push(40)
+        L_List.push(50)
+
+        print_reverse = LL_Problems(L_List)
+        print(print_reverse.print_nodes_from_end())
+
+    # print_nodes_from_end_test()
+
+    def check_length():
+        L_List = SinglyLinkedList()
+        L_List.push(10)
+        L_List.push(20)
+        L_List.push(30)
+        L_List.push(40)
+        L_List.push(50)
+
+
+        find_Length = LL_Problems(L_List)
+        find_Length.check_length()
+
+    check_length()
